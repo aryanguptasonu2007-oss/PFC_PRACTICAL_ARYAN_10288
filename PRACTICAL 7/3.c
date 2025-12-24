e8c67599fc55a470c3c74ad202172feb3af0184d
aryanguptasonu2007-oss <aryanguptasonu2007@gmail.com>
2025-12-24 23:56:38 +0530

    Add files via upload

---
+++ PRACTICAL 7/3.c
@@ -0,0 +1,20 @@
+// ARYAN GUPTA
+// ERP 10288
+#include <stdio.h>
+int main() {
+    int n=4;
+    for(int i=1;i<=n;i++){
+        for(char ch='A'; ch<'A'+i; ch++){
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+/* OUTPUT:
+A
+AB
+ABC
+ABCD
+*/
---
+++ PRACTICAL 8/3.c
@@ -0,0 +1,20 @@
+// ARYAN GUPTA
+// ERP 10288
+#include <stdio.h>
+int main(){
+    int n=4;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+/* OUTPUT for n=4:
+*
+**
+***
+****
+*/
